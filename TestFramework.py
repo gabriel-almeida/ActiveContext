48f9510edfff7e382c61e4fa7fe6f8343edbb7f4
gabriel-almeida <gabriel.almeida.cp2@gmail.com>
2016-02-02 18:58:10 -0200

    parallel test

--- TestFramework.py
+++ TestFramework.py
@@ -7,6 +7,8 @@ import mae
 from OneHotEncoder import OneHotEncoder
 from Selectors import LargestDeviationContextSelection, AllContextSelection,\
     CramerLargestDeviation, RandomContextSelection
+import copy
+import multiprocessing
 from scipy.stats import ttest_ind
 
 class TestFramework():
@@ -26,10 +28,29 @@ class TestFramework():
         self.ids_candidate = ids[train_split:candidate_split]
         self.ids_test = ids[candidate_split:]
 
+    def _test_selector(self, selector_item):
+        selector = selector_item[1]
+        selector_name = selector_item[0]
+
+        selector.obtain_initial_train(self.dataset[self.ids_train, :], self.context[self.ids_train, :], self.n_context_choice)
+        for candidate in self.ids_candidate:
+            chosen_contexts = selector.choose_contexts(self.dataset[candidate, :])
+            selector.obtain_contextual_train_sample(self.dataset[candidate, :], self.context[candidate, chosen_contexts], chosen_contexts)
+
+        responses = []
+        for test in self.ids_test:
+            # chosen_contexts = selector.choose_contexts(self.dataset[test, :])
+            # prediction = selector.obtain_contextual_test_sample(self.dataset[test, :], self.context[test, chosen_contexts])
+            prediction = selector.obtain_contextual_test_sample(self.dataset[test, :], self.context[test, :])
+            responses.append(prediction)
+
+        y_hat = np.array(responses)
+        actual_mae = mae.MAE().calculate(self.dataset[self.ids_test, :], y_hat)
+        return (selector_name, actual_mae)
+
     def test_procedure(self, n_context_choice, context_selectors, n_repetitions=20):
         (nSample, nFeature) = np.shape(self.dataset)
-
-        arq = open("debug.csv", 'w')
+        self.n_context_choice = n_context_choice
 
         #cast single object to list
         if type(context_selectors) is not dict:
@@ -40,30 +61,16 @@ class TestFramework():
         for name in context_selectors.keys():
             results_by_algorithm[name] = []
 
+        pool = multiprocessing.Pool(len(context_selectors))
         for repetition in range(n_repetitions):
             print(repetition + 1)
             self.__prepare_holdout(nSample)
-            for selector_name, selector in context_selectors.items():
-                selector.obtain_initial_train(self.dataset[self.ids_train, :], self.context[self.ids_train, :], n_context_choice)
 
-                arq.write(selector_name)
-                for candidate in self.ids_candidate:
-                    chosen_contexts = selector.choose_contexts(self.dataset[candidate, :])
-                    selector.obtain_contextual_train_sample(self.dataset[candidate, :], self.context[candidate, chosen_contexts], chosen_contexts)
-                    arq.write(str(chosen_contexts))
-                    arq.write(' ')
-                arq.write('\n')
+            #parallel execution
+            results = pool.map(self._test_selector, context_selectors.items())
 
-                responses = []
-                for test in self.ids_test:
-                    # chosen_contexts = selector.choose_contexts(self.dataset[test, :])
-                    # prediction = selector.obtain_contextual_test_sample(self.dataset[test, :], self.context[test, chosen_contexts])
-                    prediction = selector.obtain_contextual_test_sample(self.dataset[test, :], self.context[test, :])
-                    responses.append(prediction)
-
-                y_hat = np.array(responses)
-                actual_mae = mae.MAE().calculate(dataset[self.ids_test, :], y_hat)
-
+            #result gathering
+            for (selector_name, actual_mae) in results:
                 print(selector_name, actual_mae)
                 results_by_algorithm[selector_name].append(actual_mae)
 
@@ -98,8 +105,8 @@ if __name__ == "__main__":
     file = "MRMR_data.csv"
 
     m = pd.read_csv(file)
-    n_context_choice = 2
-    n_repetitions = 1
+    n_context_choice = 3
+    n_repetitions = 5
     dataset = m.values[:, 0:3]
     context = m.values[:, 7: 19]
 
@@ -109,10 +116,11 @@ if __name__ == "__main__":
     svd = ContextualSVD.ContextualSVD(n_user, n_item, max_steps=100, n_latent_features=20, mode='item')
     encoder = OneHotEncoder(context, na_value=-1)
 
-    largest_deviation = LargestDeviationContextSelection(svd, encoder)
-    random_choice = RandomContextSelection(svd, encoder)
-    baseline = AllContextSelection(svd, encoder)
-    cramer = CramerLargestDeviation(svd, encoder)
+    largest_deviation = LargestDeviationContextSelection(copy.deepcopy(svd), encoder)
+    random_choice = RandomContextSelection(copy.deepcopy(svd), encoder)
+    baseline = AllContextSelection(copy.deepcopy(svd), encoder)
+    cramer = CramerLargestDeviation(copy.deepcopy(svd), encoder)
+
     selectors = {"Largest Deviation": largest_deviation, "Random": random_choice, "All Contexts": baseline,
                  "Cramer Deviation": cramer}
 
